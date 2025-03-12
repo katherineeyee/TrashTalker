08a96fd0494f3cd67ccc746a37d892e8424f5b9a
Hninyeeko <hninyeeko.apple@gmail.com>
2025-03-11 22:11:35 -0700

    adding routes to Glass, Plastic and Compost pages in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Glass from './pages/Glass';
+import Plastic from './pages/Plastic';
+import Compost from './pages/Compost';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/Glass" element={<Glass />} />
+        <Route path="/Plastic" element={<Plastic />} />
+        <Route path="/Compost" element={<Compost />} />
         {/* Add more routes as needed */}
       </Routes>
     </Router>
