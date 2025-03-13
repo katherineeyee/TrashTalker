d9b4af226850e9ed0cd8a09be7a00d4f6daf5478
vantrinh1207 <vuaivan.trinh@sjsu.edu>
2025-03-12 19:06:56 -0700

    update landing add common material drop down

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
 // src/pages/HomePage.jsx
-import React from 'react';
-import Navbar from '../components/Navbar';
-import LandingSection from '../components/LandingSection';
-import Features from '../components/Features';
+import React from "react";
+import Navbar from "../components/Navbar";
+import LandingSection from "../components/LandingSection";
+import Features from "../components/Features";
 
 const HomePage = () => {
   return (
