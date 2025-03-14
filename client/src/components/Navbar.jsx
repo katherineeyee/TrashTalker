36c641298f71a1a787eb17940ef82f49249dc9c6
RileyShort1 <rileyshort1@gmail.com>
2025-03-13 22:01:58 -0700

    Added link to home page in TrashTalker logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,11 +5,13 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center py-2 px-4 bg-white border-b border-gray-200">
       <div className="flex items-center">
-        <img 
-          src="/TrashTalkerLogo.png"
-          alt="TrashTalker Logo"
-          className="h-16"
-        />
+        <Link to="/">
+          <img
+            src="/TrashTalkerLogo.png"
+            alt="TrashTalker Logo"
+            className="h-16"
+          />
+        </Link>
       </div>
       <div className="flex items-center gap-6">
         <Link to="/" className="text-gray-700 hover:text-gray-900">Home</Link>
