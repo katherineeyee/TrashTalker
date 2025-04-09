e59900cd74d10b22524f3479207b4a7731b05ad1
Riley <rileyshort1@gmail.com>
2025-04-08 21:04:51 -0700

    Added mongoDB server connection and test API

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 // server/server.js
 const express = require('express');
-//const mongoose = require('mongoose'); //temp commented out bc we havent set up mongodb yet
 const cors = require('cors');
 const morgan = require('morgan');
-//require('dotenv').config(); //temp commented out bc we havent set up mongodb yet
 
 const app = express();
 const PORT = process.env.PORT || 5001; //changed port no. from 5000 to 5001
@@ -13,18 +11,25 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
-// Connect to MongoDB
-//temp commented out bc we havent set up mongodb yet
-//mongoose.connect(process.env.MONGODB_URI)
-//  .then(() => console.log('MongoDB connected...'))
-//  .catch(err => console.log(err));
+const MongoDbURI = 'mongodb+srv://TrashTalker:055ECBBFF0F0FE62161096C6D4A32EBD@trashtalkercluster.lygarak.mongodb.net/TrashTalkerDB?retryWrites=true&w=majority&appName=TrashTalkerCluster'
 
-// Routes (to be added)
-app.use('/api/users', require('./routes/users'));
+const mongoose = require("mongoose");
+mongoose.connect(MongoDbURI)
+    .then(() => console.log("Connected to TrashTalker MongoDB database"))
+    .catch(err => console.log("Error connecting to database", err));
 
-// Basic route
-app.get('/', (req, res) => {
-  res.send('API is running...');
+
+// Routes
+const Test = require("./models/Test");
+
+app.get("/api/tests", async (req, res) => {
+  try {
+    const tests = await Test.find();
+    res.json(tests);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
 });
 
 // Start server
