cbe04be0be64c71b24dff35b27a5f89a5038800e
vantrinh1207 <vuaivan.trinh@sjsu.edu>
2025-04-13 04:37:43 -0700

    someinstruction

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import LandingSection from "../components/LandingSection";
 import Features from "../components/Features";
-
+import HowItWorks from "../components/HowItWorks";
+import Gamification from "../components/Gamification";
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-gray-50">
@@ -11,6 +12,8 @@ const HomePage = () => {
       <div className="container mx-auto">
         <LandingSection />
         <Features />
+        <HowItWorks />
+        <Gamification />
       </div>
     </div>
   );
